2f51c6dcb0dac36838c348825c982a3be5312799
Ankit Singh Chauhan <ankitcloud2563@gmail.com>
2024-07-15 00:18:00 +0530

    adding thee myscript.js

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -1,1 +1,12 @@
 /** @format */
+function changeColor(element) {
+  Array.from(document.getElementsByTagName("li")).forEach((ele) => {
+    ele.getElementsByTagName("a")[0].style.color = "var(--text-black-900)";
+    console.log(ele);
+  });
+  element.style.color = "var(--skin-color)";
+  console.log("vai");
+}
+
+
+
